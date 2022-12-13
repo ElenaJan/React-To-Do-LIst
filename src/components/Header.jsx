446fe62773b9352c279d684c93f4b23d4a16e90f
Elena Janecek <janechek.elena@gmail.com>
2022-12-13 15:07:11 +0300

    add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import '../styles/Header.sass';
+
+const Header = () => {
+    return(
+        <header className="header">
+            <h2 className='header__title'>Список дел</h2>
+        </header>
+    )
+}
+
+export default Header;
