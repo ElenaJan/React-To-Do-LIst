898c7ecc867c4f145265bc0d4dce67940336e9e8
Elena Janecek <janechek.elena@gmail.com>
2022-12-13 15:06:45 +0300

    change Alert

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,12 +1,13 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
+import '../styles/Alert.sass'
 
-const Alert = ({type, message, removeAlert, list}) => {
+const Alert = ({type, message, removeAlert, todos}) => {
     useEffect(() => {
         const timeout = setTimeout(() => {
             removeAlert();
         }, 5000);
         return() => clearTimeout(timeout);
-    }, [list])
+    }, [todos])
   return (
     <p className={`alert alert-${type}`}>{message}</p>
   )
